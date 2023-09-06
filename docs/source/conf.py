e91cbbd6a44deb4fd56a938f10e40863eafaf28a
Morgan Lethuillier <morgan@in2p3.fr>
2023-09-06 16:33:45 +0200

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 html_show_sphinx = False
 html_show_copyright = False
-html_show_sourcelink = False
+#html_show_sourcelink = False
 
 
 # -- Options for EPUB output
