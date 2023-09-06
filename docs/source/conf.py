6592ad74381ee83c6aecdfb204ba6f02e4162e4b
Morgan Lethuillier <morgan@in2p3.fr>
2023-09-06 13:33:16 +0200

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Doc for MBTA offline prod'
+copyright = '2023, ML'
+author = 'morgan.lethuillier@ligo.org'
 
 release = '0.1'
 version = '0.1.0'
